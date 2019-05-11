809189dbcb28560dc067ea48a5ff8ec125f960ef
Jane Adelmann <jane.adelmann@plagiarismcheck.org>
2019-05-11 14:42:51 +0300

    Fix issue #23. Delete concatenation operator.

--- version.php
+++ version.php
@@ -26,9 +26,9 @@ defined('MOODLE_INTERNAL') || die();
 if (!isset($plugin)) {
     $plugin = new stdClass();
 }
-$plugin->version = 2019042601;
+$plugin->version = 2019051101;
 $plugin->requires = 2017051501; // Requires Moodle 3.3 .
-$plugin->release = 'v3.5';
+$plugin->release = 'v3.6';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'plagiarism_pchkorg';
 $plugin->dependencies = array(
