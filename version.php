600676c990cf47e51ee5b161f34f574ccfd0a02a
Jane Adelmann <jane.adelmann@plagiarismcheck.org>
2019-05-27 12:12:46 +0300

    Version 3.6.1 Add support for moodle 3.3

--- version.php
+++ version.php
@@ -26,9 +26,9 @@ defined('MOODLE_INTERNAL') || die();
 if (!isset($plugin)) {
     $plugin = new stdClass();
 }
-$plugin->version = 2019051101;
-$plugin->requires = 2017051501; // Requires Moodle 3.3 .
-$plugin->release = 'v3.6';
+$plugin->version = 2019052701;
+$plugin->requires = 2017051500; // Requires Moodle 3.3 .
+$plugin->release = 'v3.6.1';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'plagiarism_pchkorg';
 $plugin->dependencies = array(
