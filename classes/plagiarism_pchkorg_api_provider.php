e198855544e9371a93a0933d970044c578c45291
Jane Adelmann <jane.adelmann@plagiarismcheck.org>
2022-11-04 17:21:10 +0200

    Add case insensitive for user email

--- classes/plagiarism_pchkorg_api_provider.php
+++ classes/plagiarism_pchkorg_api_provider.php
@@ -455,7 +455,7 @@ class plagiarism_pchkorg_api_provider {
      */
     public function user_email_to_hash($email) {
         // We don't send raw user email to the service.
-        return hash('sha256', $this->token . $email);
+        return hash('sha256', $this->token . strtolower($email));
     }
 
     /**
