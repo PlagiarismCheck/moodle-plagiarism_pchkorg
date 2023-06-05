PlagiarismCheck/moodle-plagiarism_pchkorg

commit 8862dc050afd992607a3196d2669d3fe5f5a73d9

Support student custom roles v3.13.4

lib.php
@@ -329,8 +329,7 @@ class plagiarism_plugin_pchkorg extends plagiarism_plugin {
             $roles[] = strtolower($rolesData->shortname);
         }
         // Moodle has multiple roles in courses.
-        $isstudent = in_array('student', $roles)
-            && !in_array('teacher', $roles)
+        $isstudent = !in_array('teacher', $roles)
             && !in_array('editingteacher', $roles)
             && !in_array('managerteacher', $roles);
 

version.php
@@ -26,9 +26,9 @@ defined('MOODLE_INTERNAL') || die();
 if (!isset($plugin)) {
     $plugin = new stdClass();
 }
-$plugin->version = 2022111616;
+$plugin->version = 2023050117;
 $plugin->requires = 2020061501; // Requires Moodle 3.9 .
-$plugin->release = 'v3.13.3';
+$plugin->release = 'v3.13.4';
 $plugin->component = 'plagiarism_pchkorg';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(

commit 8ba5f460f9a74a251990f4af807266d4a7090aec

Update lib.php "Support student custom roles"
8862dc0 introduced a bug that caused the widget to be disabled for an excess of Moodle user roles, as they became considered "Students" as per $isstudent 

This change I believe was intentional, but to be applied where $isstudent is reassigned for the purpose of $role in auto_registrate_member($name, $email, $role)

I believe that this would have had the intended effect of avoiding real world students that don't have the specific named role 'student' on Moodle (i.e. having a custom role that uses the 'student' archetype but a different role name) from being given 'Teacher' role and permissions on plagiarismcheck.org

lib.php
@@ -329,7 +329,8 @@ class plagiarism_plugin_pchkorg extends plagiarism_plugin {
             $roles[] = strtolower($rolesData->shortname);
         }
         // Moodle has multiple roles in courses.
-        $isstudent = !in_array('teacher', $roles)
+        $isstudent = in_array('student', $roles)
+            && !in_array('teacher', $roles)
             && !in_array('editingteacher', $roles)
             && !in_array('managerteacher', $roles);
 
@@ -742,10 +743,9 @@ display: inline-block;"
                     $roles[] = strtolower($rolesData->shortname);
                 }
                 // Moodle has multiple roles in courses.
-                $isstudent = in_array('student', $roles)
-                    && !in_array('teacher', $roles)
-                    && !in_array('editingteacher', $roles)
-                    && !in_array('managerteacher', $roles);
+                $isstudent = !in_array('teacher', $roles)
+					&& !in_array('editingteacher', $roles)
+					&& !in_array('managerteacher', $roles);
                 $isregistered = $apiprovider->auto_registrate_member($name, $USER->email, $isstudent ? 3 : 2);
                 if (!$isregistered) {
                     return true;