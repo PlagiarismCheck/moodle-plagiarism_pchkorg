830b77e6f7de9383a347f158560deac77a3e28cc
Jane Adelmann <jane.adelmann@plagiarismcheck.org>
2023-06-23 16:39:20 +0300

    Fix for database schema

--- version.php
+++ version.php
@@ -26,9 +26,9 @@ defined('MOODLE_INTERNAL') || die();
 if (!isset($plugin)) {
     $plugin = new stdClass();
 }
-$plugin->version = 2023060812;
+$plugin->version = 2023062316;
 $plugin->requires = 2020061501; // Requires Moodle 3.9 .
-$plugin->release = 'v3.14.2';
+$plugin->release = 'v3.14.3';
 $plugin->component = 'plagiarism_pchkorg';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
