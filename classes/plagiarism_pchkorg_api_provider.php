fdf5bf64585a974d2d4846fb00fb56f9696e190c
Jane Adelmann <jane.adelmann@plagiarismcheck.org>
2025-01-16 14:14:34 +0200

    v3.15.9 Fix situation on queue is stuck when more then 20 documents failed. For group account.

--- classes/plagiarism_pchkorg_api_provider.php
+++ classes/plagiarism_pchkorg_api_provider.php
@@ -643,11 +643,17 @@ class plagiarism_pchkorg_api_provider {
                 ),
         ));
         if ($json = json_decode($response)) {
-            if (isset($json->data) && 5 == $json->data->state) {
+            if (isset($json->data) && \in_array($json->data->state, array(4,5,6,7,8), true)) {
                 $result = new stdClass;
                 $result->id = $json->data->report->id;
-                $result->percent = $json->data->report->percent;
-                $result->percent_ai = $json->data->ai_report->processed_percent;
+                $result->state = $json->data->state;
+                if (5 === $json->data->state) {
+                    $result->percent = $json->data->report->percent;
+                    $result->percent_ai = $json->data->ai_report->processed_percent;
+                } else {
+                    $result->percent = null;
+                    $result->percent_ai = null;
+                }
 
                 return $result;
             }
--- version.php
+++ version.php
@@ -26,9 +26,9 @@ defined('MOODLE_INTERNAL') || die();
 if (!isset($plugin)) {
     $plugin = new stdClass();
 }
-$plugin->version = 2025011614;
+$plugin->version = 2025011615;
 $plugin->requires = 2020061501; // Requires Moodle 3.9 .
-$plugin->release = 'v3.15.8';
+$plugin->release = 'v3.15.9';
 $plugin->component = 'plagiarism_pchkorg';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
