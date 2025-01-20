50a85f28959eba24eb1531492d128be0de198187
Jane Adelmann <jane.adelmann@plagiarismcheck.org>
2025-01-20 12:59:19 +0200

    Small fix for xmldb schema

--- version.php
+++ version.php
@@ -26,9 +26,9 @@ defined('MOODLE_INTERNAL') || die();
 if (!isset($plugin)) {
     $plugin = new stdClass();
 }
-$plugin->version = 2025011713;
+$plugin->version = 2025012012;
 $plugin->requires = 2020061501; // Requires Moodle 3.9 .
-$plugin->release = 'v3.15.11';
+$plugin->release = 'v3.15.12';
 $plugin->component = 'plagiarism_pchkorg';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
